fd6e58d8051502a6ad84ffc488c88b1ee7df93d4
Kyle Fiegener <KyleFiegener@gmail.com>
2017-12-21 09:12:58 -0800

    Renamed db because PLANS >:}

--- hfsite/settings.py
+++ hfsite/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'hfsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'hfapp', 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'hfapp', 'master.sqlite3'),
     }
 }
 
